574c72a7829268f7061486efcb2c8b49ad8f1b63
MuhammadAan18 <Muhammadaan796@gmail.com>
2024-12-18 21:47:17 +0800

    perbaikan sorting dan penambahan searching

--- Graphopia/src/sortingPengunjung.java
+++ Graphopia/src/sortingPengunjung.java
@@ -1,27 +1,78 @@
-import java.util.ArrayList;
-
 public class sortingPengunjung {
-    public void jenisTiket(ArrayList<Passenger> passenger){
-        for(int i=0; i< passenger.size() - 1; i++){
-            for(int j=0; j< passenger.size()-i-1; j++){
-                Passenger current = passenger.get(j);
-                Passenger next = passenger.get(j+1);
+    private Node<Passenger> head;
 
-                if (!current.isVIP && next.isVIP) {
-                    passenger.set(j, next);
-                    passenger.set(j+1, current);
+    public int size(){
+        int hitung = 0;
+        Node<Passenger> current = head;
+        while(current != null){
+            hitung++;
+            current = current.next;
+        }
+        return hitung;
+    }
+    public Node<Passenger> urutanNode(int index){
+        int i = 0;
+        Node<Passenger> current = head;
+        while (current != null) {
+            if (i == index) {
+                return current;
+            }
+            current = current.next;
+            i++;
+        }
+        return null;
+    }
+
+    public void swap(int index1, int index2) {
+        if (index1 == index2){
+            return;
+        } 
+
+        Node<Passenger> node1 = urutanNode(index1);
+        Node<Passenger> node2 = urutanNode(index2);
+
+        if (node1 != null && node2 != null) {
+            Passenger temp = node1.data;
+            node1.data = node2.data;
+            node2.data = temp;
+        }
+    }
+
+    public class SortingPengunjung {
+        public void jenisTiket() {
+            int n = size();
+            for (int i = 0; i < n - 1; i++) {
+                for (int j = 0; j < n - i - 1; j++) {
+                    Node<Passenger> current = urutanNode(j);
+                    Node<Passenger> next = urutanNode(j + 1);
+    
+                    if (current != null && next != null && !current.data.isVIP && next.data.isVIP) {
+                        swap(j, j + 1);
+                    }
                 }
-
             }
         }
     }
 
-    public void displayAntrian(ArrayList<Passenger> passengers){
+    public void displayAntrian(Node <Passenger> pengunjung){
         int no = 1;
         System.out.println("=================GRAPHOPIA=================\n===============List Penumpang===============");
-        for(Passenger p : passengers){
-            System.out.println(no +". " + p);
+        Node<Passenger> current = head;
+        while (current != null) {
+            System.out.println(no + ". " + current.data);
+            current = current.next;
             no++;
         }
+    }
+
+    public Passenger searchingName(String name){
+        Node<Passenger> current = head;
+        while (current != null) {
+            if (current.data.name.equalsIgnoreCase(name)) {
+                System.out.println("Nama Ditemukan\n" + current.data);    
+            }
+            current = current.next;
+        }
+        return null;
     }
 }
